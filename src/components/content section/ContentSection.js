a42423258c960bb5a19d710fde4fd96d700513d0
saeed <saem1721364@gmail.com>
2024-06-29 10:12:12 +0330

    Add content-section compon

---
+++ src/components/content section/ContentSection.js
@@ -0,0 +1,12 @@
+// src/components/content section/ContentSection
+
+const ContentSection = React.forwardRef(({ section, index }, ref) => {
+  return (
+    <div>
+      <h2 className="text-3xl">{section}</h2>
+      <div ref={ref} className="min-h-screen flex items-center justify-center bg-gray-200 my-2"></div>
+    </div>
+  );
+});
+
+export default ContentSection;
